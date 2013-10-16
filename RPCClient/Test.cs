de35d9775fd8b795997b557ec9aa476157c68aa8
Elbandi <elso.andras@gmail.com>
2013-10-16 15:22:37 +0200

    Fix build

--- RPCClient/Test.cs
+++ RPCClient/Test.cs
@@ -8,7 +8,6 @@ namespace BitcoinNET.RPCClient
 	{
 		static void Main(string[] args)
 		{
-<<<<<<< HEAD
 			BatchRPC r = new BatchRPC(new Uri("http://10.1.1.224:8332"), new NetworkCredential("rpcuser1", "rpcpass1"));
 
 			uint _tx = r.ListTransactions();
@@ -18,11 +17,9 @@ namespace BitcoinNET.RPCClient
 			r.DoRequest();
 
 			var tx = r.GetResult<IEnumerable<ListTransactionsResponse>>(_tx);
-=======
 			BitcoinRPC b = new BitcoinRPC(new Uri("http://127.0.0.1:8332"), new NetworkCredential("rpcuser", "rpcpass"));
 
-			var r = b.CreateMultiSig(1, new String[] { "1N71zTS2S17FJTctT6Xwna5YuioQ6iy4aT", "1N71zTS2S17FJTctT6Xwna5YuioQ6iy4aT" });
->>>>>>> 7c77783... implimenting 0.8 commands
+			var r2 = b.CreateMultiSig(1, new String[] { "1N71zTS2S17FJTctT6Xwna5YuioQ6iy4aT", "1N71zTS2S17FJTctT6Xwna5YuioQ6iy4aT" });
 
 			Console.WriteLine(tx);
 
